6a1027279600c4bc4b4ac17415c66e7f805a2a7e
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-01-20 12:23:56 -0500

    dgraph:20.7.3

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+set -e
 DIR=~/Downloads
 MIRROR=https://github.com/dgraph-io/dgraph/releases/download
 
@@ -7,9 +8,10 @@ dl()
     local ver=$1
     local os=$2
     local arch=$3
+    local archive_type=${4:-tar.gz}
     local platform="${os}-${arch}"
-    local url=$MIRROR/$ver/dgraph-${platform}.tar.gz
-    local lfile=$DIR/dgraph-${platform}-${ver}.tar.gz
+    local url=$MIRROR/$ver/dgraph-${platform}.${archive_type}
+    local lfile=$DIR/dgraph-${platform}-${ver}.${archive_type}
     if [ ! -e $lfile ];
     then
         wget -q -O $lfile $url
@@ -23,7 +25,7 @@ dl_ver() {
     printf "  %s:\n" $ver
     dl $ver darwin amd64
     dl $ver linux amd64
-    dl $ver windows amd64
+    dl $ver windows amd64 zip
 }
 
-dl_ver ${1:-v20.07.2}
+dl_ver ${1:-v20.07.3}
