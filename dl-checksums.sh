34ca2e8c478dde56d80d767c58c3cd15d4829127
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-11-22 23:17:08 -0500

    break out badger and dgraph

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -6,24 +6,28 @@ MIRROR=https://github.com/dgraph-io/dgraph/releases/download
 dl()
 {
     local ver=$1
-    local os=$2
-    local arch=$3
-    local archive_type=${4:-tar.gz}
+    local app=$2
+    local os=$3
+    local arch=$4
+    local archive_type=${5:-tar.gz}
     local platform="${os}-${arch}"
-    local url=$MIRROR/$ver/dgraph-${platform}.${archive_type}
-    local lfile=$DIR/dgraph-${platform}-${ver}.${archive_type}
+    local url=$MIRROR/$ver/${app}-${platform}.${archive_type}
+    local lfile=$DIR/${app}-${platform}-${ver}.${archive_type}
     if [ ! -e $lfile ];
     then
         curl -sSLf -o $lfile $url
     fi
-    printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+    printf "      # %s\n" $url
+    printf "      %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
 }
 
 dl_ver() {
     local ver=$1
     printf "  %s:\n" $ver
-    dl $ver linux amd64
+    printf "    dgraph:\n"
+    dl $ver dgraph linux amd64
+    printf "    badger:\n"
+    dl $ver badger linux amd64
 }
 
-dl_ver ${1:-v21.12.0}
+dl_ver ${1:-v22.0.1}
